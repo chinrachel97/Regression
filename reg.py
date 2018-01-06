a5b5fd5533dfa70778b1375d36501915b6f7b054
chinrachel97 <chinrachel97@gmail.com>
2018-01-05 23:41:09 -0600

    initial commit

---
+++ reg.py
@@ -0,0 +1,54 @@
+from __future__ import print_function
+import numpy as np
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Dropout
+from keras.optimizers import SGD, RMSprop, Adam
+from keras.utils import np_utils
+import csv
+
+np.random.seed(1671) #for reproducibility
+
+#network and training
+NB_EPOCH = 200
+BATCH_SIZE = 32
+VERBOSE = 1
+NB_CLASSES = 1 #number of outputs
+OPTIMIZER = Adam() #optimizer
+N_HIDDEN = 128
+VALIDATION_SPLIT = 0.2 #how much TRAIN is reserved for validation
+DROPOUT = 0.3
+
+#read the file
+filename = 'fst_degree.csv'
+raw_data = open(filename, 'r')
+reader = csv.reader(raw_data)
+headers = next(reader)
+
+#load dataset
+dataset = np.loadtxt(raw_data, delimiter=",")
+
+#split into input and output variables
+X = dataset[:, 0]
+Y = dataset[:, 1]
+
+model = Sequential()
+model.add(Dense(NB_CLASSES, input_shape=(1,)))
+model.add(Activation('relu'))
+
+model.summary()
+
+#compile model
+model.compile(loss='mean_squared_error', optimizer=OPTIMIZER, metrics=['accuracy'])
+
+#fit the model
+history = model.fit(X, Y, batch_size=BATCH_SIZE, epochs=NB_EPOCH,
+verbose=VERBOSE, validation_split=VALIDATION_SPLIT)
+
+score = model.evaluate(X, Y, verbose=VERBOSE)
+print("Test score:", score[0])
+print("Test accuracy:", score[1])
+
+#calculate predictions
+to_predict = np.array([101,120])
+predictions = model.predict(to_predict)
+print("Prediction:", predictions)
