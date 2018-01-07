c570d246bdc0c60db2f7bc2d82c129a10e6f5f15
chinrachel97 <chinrachel97@gmail.com>
2018-01-07 02:12:33 -0600

    added two more functions

--- rand_generator.py
+++ rand_generator.py
@@ -1,16 +1,50 @@
 import csv
 import random
 
+#f(x)=x+50
+with open('lin_values.csv', 'w', newline='') as csvfile:
+	w = csv.writer(csvfile, delimiter=',')
+	for i in range(20000):
+		x = random.randint(1, 1001)
+		y = x+50
+		w.writerow([x/1000, y/1000])
+		
+with open('lin_values_test.csv', 'w', newline='') as csvfile:
+	w = csv.writer(csvfile, delimiter=',')
+	for i in range(10000):
+		x = random.randint(1, 1001)
+		y = x+50
+		w.writerow([x/1000, y/1000])
+
+#f(x)=x^2
 with open('quad_values.csv', 'w', newline='') as csvfile:
+	w = csv.writer(csvfile, delimiter=',')
+	for i in range(20000):
+		x = random.randint(1, 1001)
+		y = x*x
+		w.writerow([x/1000, y/1000000])
+		
+
+with open('quad_values_test.csv', 'w', newline='') as csvfile:
 	w = csv.writer(csvfile, delimiter=',')
 	for i in range(10000):
 		x = random.randint(1, 1001)
 		y = x*x
-		w.writerow([x, y])
+		w.writerow([x/1000, y/1000000])
 		
-with open('lin_values.csv', 'w', newline='') as csvfile:
+#f(x)=x^3 + 2*x
+with open('cube_values.csv', 'w', newline='') as csvfile:
+	w = csv.writer(csvfile, delimiter=',')
+	for i in range(20000):
+		x = random.randint(1, 1001)
+		y = pow(x,3) + (2*x)
+		w.writerow([x/1000, y/1000000000])
+		
+
+with open('cube_values_test.csv', 'w', newline='') as csvfile:
 	w = csv.writer(csvfile, delimiter=',')
 	for i in range(10000):
 		x = random.randint(1, 1001)
-		y = x+1
-		w.writerow([x, y])
+		y = pow(x,3) + (2*x)
+		w.writerow([x/1000, y/1000000000])
+		
